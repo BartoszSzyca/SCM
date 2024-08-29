4883b77f5e386abbe16be764b6c609c669735d5b
Bitrax <120678297+BartoszSzyca@users.noreply.github.com>
2024-08-29 20:28:35 +0200

    Update Food class formatting and __repr__ method

--- core/food.py
+++ core/food.py
@@ -1,12 +1,16 @@
 from typing import Dict, Optional, Union
 from datetime import date
 
-class Food:
-    def __init__(self, name: str, quantities: Dict[str, Union[int, float]], price: float = 0.0, 
-                 macros: Optional[Dict[str, float]] = None, micros: Optional[Dict[str, float]] = None,
-                 purchase_date: Optional[date] = None, expiration_date: Optional[date] = None, 
-                 storage_conditions: Optional[str] = None, description: Optional[str] = None):
 
+class Food:
+    def __init__(self, name: str, quantities: Dict[str, Union[int, float]],
+                 price: float = 0.0,
+                 macros: Optional[Dict[str, float]] = None,
+                 micros: Optional[Dict[str, float]] = None,
+                 purchase_date: Optional[date] = None,
+                 expiration_date: Optional[date] = None,
+                 storage_conditions: Optional[str] = None,
+                 description: Optional[str] = None):
         self.name = name
         self.quantities = quantities
         self.price = price
@@ -16,9 +20,15 @@ class Food:
         self.expiration_date = expiration_date
         self.storage_conditions = storage_conditions
         self.description = description
-                     
+
     def __repr__(self):
-        return (f"<Food(name={self.name}, quantities={self.quantities}, price={self.price:.2f}, "
-                f"macros={self.macros}, micros={self.micros}, purchase_date={self.purchase_date}, "
-                f"expiration_date={self.expiration_date}, storage_conditions={self.storage_conditions}, "
-                f"description={self.description})>")
+        return (
+            f"<Food(name={self.name}, "
+            f"quantities={self.quantities}, "
+            f"price={self.price:.2f}, "
+            f"macros={self.macros}, "
+            f"micros={self.micros}, "
+            f"purchase_date={self.purchase_date}, "
+            f"expiration_date={self.expiration_date}, "
+            f"storage_conditions={self.storage_conditions}, "
+            f"description={self.description})>")
