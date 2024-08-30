0a4ffce5847833aea8ee0fcf4321b4401ace21d8
Bitrax <120678297+BartoszSzyca@users.noreply.github.com>
2024-08-30 08:40:55 +0200

    Update database.py Add handling for existing food items with update option
    - Modified `add_food_to_db` function to check if a food item already exists in the database.
    - If the food item exists, prompt the user to update the existing record.
    - Added `update_food_in_db` function to handle updating of existing food items.
    - If the user chooses not to update, the operation is canceled.
    - Retained the functionality to add new food items if they don't exist.

--- database/database.py
+++ database/database.py
@@ -11,20 +11,43 @@ session = Session()
 
 
 def add_food_to_db(food: Food):
-    food_model = FoodModel(
-        name=food.name,
-        quantities=food.quantities,
-        price=food.price,
-        macros=food.macros,
-        micros=food.micros,
-        purchase_date=food.purchase_date,
-        expiration_date=food.expiration_date,
-        storage_conditions=food.storage_conditions,
-        description=food.description)
+    existing_food = session.query(FoodModel).filter_by(name=food.name).first()
 
-    session.add(food_model)
+    if existing_food:
+        print(f"Produkt o nazwie '{food.name}' już istnieje w bazie danych.")
+        choice = input("Nadpisać go? (Y/N)")
+        if choice.upper() == "Y":
+            update_food_in_db(food, existing_food)
+    else:
+        food_model = FoodModel(
+            name=food.name,
+            quantities=food.quantities,
+            price=food.price,
+            macros=food.macros,
+            micros=food.micros,
+            purchase_date=food.purchase_date,
+            expiration_date=food.expiration_date,
+            storage_conditions=food.storage_conditions,
+            description=food.description)
+
+        session.add(food_model)
+        session.commit()
+        return food_model
+
+
+def update_food_in_db(food: Food, existing_food):
+    existing_food.quantities = food.quantities
+    existing_food.price = food.price
+    existing_food.macros = food.macros
+    existing_food.micros = food.micros
+    existing_food.purchase_date = food.purchase_date
+    existing_food.expiration_date = food.expiration_date
+    existing_food.storage_conditions = food.storage_conditions
+    existing_food.description = food.description
+
     session.commit()
-    return food_model
+    print(f"Produkt o nazwie '{food.name}' został zaktualizowany.")
+    return existing_food
 
 
 def fetch_all_foods():
