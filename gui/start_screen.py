232d1be33bce5ed6127a34753ce6c9c35448f6a7
Bitrax <120678297+BartoszSzyca@users.noreply.github.com>
2024-09-13 09:51:11 +0200

    Update start_screen.py with login/registration forms and fixed layout issues

--- gui/start_screen.py
+++ gui/start_screen.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QLineEdit, \
-    QPushButton ,QHBoxLayout
+    QPushButton, QHBoxLayout, QStackedWidget
 from PySide6.QtGui import QPixmap
 
 existing_username = "qwe"
@@ -10,12 +10,31 @@ existing_email = "qwe@wp.pl"
 class StartScreen(QWidget):
     def __init__(self):
         super().__init__()
+
         self.setWindowTitle("Smart Cart Master (SCM)")
 
         image_label = QLabel()
         image_label.setPixmap(QPixmap("images/images.png"))
 
-        #
+        self.login_screen = LoginScreen(self)
+        self.register_screen = RegisterScreen(self)
+
+        self.stack = QStackedWidget(self)
+        self.stack.addWidget(self.login_screen)
+        self.stack.addWidget(self.register_screen)
+
+        primary_v_layout = QVBoxLayout()
+        primary_v_layout.addWidget(image_label)
+        primary_v_layout.addWidget(self.stack)
+
+        self.setLayout(primary_v_layout)
+
+
+class LoginScreen(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+
+        self.parent = parent
 
         username_label_login = QLabel("Username:")
         self.username_input_login = QLineEdit()
@@ -28,9 +47,40 @@ class StartScreen(QWidget):
         login_button.clicked.connect(self.login)
 
         reqister_switch_button = QPushButton("Create an account!")
-        # reqister_switch_button.clicked.connect(self.show_register)
+        reqister_switch_button.clicked.connect(self.switch_to_register)
 
-        #
+        v_layout_login = QVBoxLayout()
+        v_layout_login.addWidget(username_label_login)
+        v_layout_login.addWidget(self.username_input_login)
+        v_layout_login.addWidget(password_label_login)
+        v_layout_login.addWidget(self.password_input_login)
+
+        h_layout_login = QHBoxLayout()
+        h_layout_login.addWidget(reqister_switch_button)
+        h_layout_login.addWidget(login_button)
+
+        v_layout_login.addLayout(h_layout_login)
+
+        self.setLayout(v_layout_login)
+
+    def login(self):
+        username = self.username_input_login.text()
+        password = self.password_input_login.text()
+
+        if username == existing_username and password == existing_password:
+            print("Login successful!")
+        else:
+            print("Login failed!")
+
+    def switch_to_register(self):
+        self.parent.stack.setCurrentWidget(self.parent.register_screen)
+
+
+class RegisterScreen(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+
+        self.parent = parent
 
         email_label_register = QLabel("Email:")
         self.email_input_register = QLineEdit()
@@ -42,27 +92,11 @@ class StartScreen(QWidget):
         self.password_input_register = QLineEdit()
         self.password_input_register.setEchoMode(QLineEdit.Password)
 
-        register_button = QPushButton("Sign in")
+        register_button = QPushButton("Register")
         register_button.clicked.connect(self.register)
 
-        switch_to_login_button = QPushButton("Login")
-        # switch_to_login_button.clicked.connect(self.show_login)
-
-        #
-
-        v_layout_login = QVBoxLayout()
-        v_layout_login.addWidget(username_label_login)
-        v_layout_login.addWidget(self.username_input_login)
-        v_layout_login.addWidget(username_label_login)
-        v_layout_login.addWidget(self.username_input_login)
-        v_layout_login.addWidget(password_label_login)
-        v_layout_login.addWidget(self.password_input_login)
-
-        h_layout_login = QHBoxLayout()
-        h_layout_login.addWidget(reqister_switch_button)
-        h_layout_login.addWidget(login_button)
-
-        #
+        switch_to_login_button = QPushButton("Back to Login")
+        switch_to_login_button.clicked.connect(self.switch_to_login)
 
         v_layout_register = QVBoxLayout()
         v_layout_register.addWidget(email_label_register)
@@ -73,26 +107,13 @@ class StartScreen(QWidget):
         v_layout_register.addWidget(self.password_input_register)
         v_layout_register.addWidget(register_button)
 
+        h_layout_register = QHBoxLayout()
+        h_layout_register.addWidget(switch_to_login_button)
+        h_layout_register.addWidget(register_button)
 
-        primary_v_layout = QVBoxLayout()
-        primary_v_layout.addWidget(image_label)
-        primary_v_layout.addWidget(image_label)
-        primary_v_layout.addLayout(v_layout_login)
-        primary_v_layout.addLayout(h_layout_login)
+        v_layout_register.addLayout(h_layout_register)
 
-        self.setLayout(primary_v_layout)
-
-
-
-
-    def login(self):
-        username = self.username_input_login.text()
-        password = self.password_input_login.text()
-
-        if username == existing_username and password == existing_password:
-            print("Login successful!")
-        else:
-            print("Login failed!")
+        self.setLayout(v_layout_register)
 
     def register(self):
         email = self.email_input_register.text()
@@ -101,8 +122,11 @@ class StartScreen(QWidget):
 
         if email != existing_email:
             if username != existing_username:
-                print("Successful!")
+                print("Registration successful!")
             else:
-                print("Username existing!")
+                print("Username already exists!")
         else:
-            print("Eamil existing!")
+            print("Email already exists!")
+
+    def switch_to_login(self):
+        self.parent.stack.setCurrentWidget(self.parent.login_screen)
