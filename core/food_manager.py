48ce7a704bfc98113b6e99ffa6397d2112d78515
Bitrax <120678297+BartoszSzyca@users.noreply.github.com>
2024-12-15 13:01:14 +0100

    Refactor: rename food.py to food_manager.py and update its functionality.

---
+++ core/food_manager.py
@@ -0,0 +1,37 @@
+from database import FoodDetailsModel, session
+
+
+class FoodManager:
+
+    def add_food_details_to_database(self, product_id, quantities, macros,
+                                     micros, storage_conditions, **details):
+        existing_food = session.query(FoodDetailsModel
+                                      ).filter_by(product_id=product_id).first()
+        if existing_food:
+            return "Food already exists"
+        new_food = FoodDetailsModel(
+            product_id=product_id,
+            quantities=quantities,
+            macros=macros,
+            micros=micros,
+            storage_conditions=storage_conditions
+        )
+        session.add(new_food)
+        session.commit()
+        return "Food added successfully"
+
+    def find_food_details(self, product_id):
+        return session.query(FoodDetailsModel).filter_by(product_id=product_id
+                                                         ).first()
+
+    def update_food_details(self, food_id, quantities, macros,
+                            micros, storage_conditions, **details):
+        food = self.find_food_details(food_id)
+        if food:
+            food.quantities = quantities
+            food.macros = macros
+            food.micros = micros
+            food.storage_conditions = storage_conditions
+            session.commit()
+        else:
+            return "Food not found"
