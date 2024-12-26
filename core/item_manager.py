552752b86474f69ea5a8907706ebdb42f1a4ca08
Bitrax <120678297+BartoszSzyca@users.noreply.github.com>
2024-12-26 11:48:02 +0100

    Update item_manager.py: Enhance ItemManager to support food and product category-specific details.

--- core/item_manager.py
+++ core/item_manager.py
@@ -1,12 +1,17 @@
-from database import session, ItemModel
+from database.database import ItemModel, session
+from core.food_manager import FoodManager
+from core.product_manager import ProductManager
 
 
 class ItemManager:
 
-    def add_item_to_database(self, category, name, description):
+    def add_item_to_database(self, category, name, description, quantities,
+                             macros, micros, storage_conditions,
+                             warranty_period, power_consumption):
         existing_item = self.find_item_in_database(name)
         if existing_item:
             return "Item already exists"
+
         new_item = ItemModel(
             category=category,
             name=name,
@@ -14,6 +19,23 @@ class ItemManager:
         )
         session.add(new_item)
         session.commit()
+        if category == "food":
+            FoodManager.add_food_details_to_database(
+                product_id=new_item.id,
+                quantities=quantities,
+                macros=macros,
+                micros=micros,
+                storage_conditions=storage_conditions
+            )
+        elif category == "product":
+            ProductManager.add_product_details_to_database(
+                product_id=new_item.id,
+                warranty_period=warranty_period,
+                power_consumption=power_consumption
+            )
+        else:
+            return "Unsuported category"
+
         return "Item added successfully"
 
     def get_all_items_from_database(self):
@@ -29,7 +51,6 @@ class ItemManager:
 
     def find_item_in_database(self, item_name):
         return session.query(ItemModel).filter_by(name=item_name).first()
-
 
     def update_item_in_database(self, item_name, category, name, description):
         item = self.find_item_in_database(item_name)
