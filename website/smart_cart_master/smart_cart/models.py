7ae3e138b96d8a4245b8d13e71de5cf7b99163d9
Bartosz (Bitrax) Szyca <Bartosz.Szyca@gmail.com>
2025-02-08 14:34:03 +0100

    feat(shopping-list): Add Django models and migrations

--- website/smart_cart_master/smart_cart/models.py
+++ website/smart_cart_master/smart_cart/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    email = models.EmailField(unique=True, null=False)
+    user_name = models.CharField(max_length=255, unique=True, null=False)
+    password = models.CharField(max_length=255,null=False)
+
+    def __str__(self):
+        return f"<id={self.user_name}(email={self.email})"
+
+
+class FoodModel(models.Model):
+    name = models.CharField(max_length=255,unique=True, null=False)
+    quantities = models.JSONField(null=False)
+    macros = models.JSONField(null=False)
+    micros = models.JSONField(null=False)
+    storage_conditions = models.CharField(max_length=1000, blank=True,
+                                          null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class FridgeModel(models.Model):
+    food = models.ForeignKey(FoodModel, on_delete=models.CASCADE,
+                             related_name="fridge_items")
+    quantities = models.IntegerField(null=False)
+    purchase_date = models.DateField(blank=True, null=True)
+    expiration_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Fridge item for {self.food.name} with {self.quantities} units"
+
+
+class ShoppingListModel(models.Model):
+    food = models.ForeignKey(FoodModel, on_delete=models.CASCADE,
+                             related_name="shopping_list")
+    quantities = models.IntegerField(null=False)
+    price = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return (f"Shopping list item: {self.food.name}, "
+                f"Quantity: {self.quantities}, "
+                f"Price: {self.price:.2f}")
